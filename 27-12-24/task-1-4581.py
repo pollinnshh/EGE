72f017f27147e63ee301bc4ecc7c3d2939dd9c64
PShevchenko <polinashvch248@gmail.com>
2024-12-27 16:49:34 +0200

    коди второго

---
+++ 27-12-24/task-1-4581.py
@@ -0,0 +1,8 @@
+from itertools import permutations
+graph = "AB AD DE EG GC CF FA FB BE".split()
+matrix = "37 367 125 56 34 247 216".split()
+
+print(*range(1,8))
+for i in permutations("ABCDEFG"):
+    if all(str(i.index(x) + 1) in matrix[i.index(y)] for x,y in graph):
+        print(*i)
