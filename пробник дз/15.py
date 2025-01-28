e7251ba89d84a9e39574ef05f26001b4a2f8c1a1
PShevchenko <polinashvch248@gmail.com>
2025-01-28 17:01:51 +0200

    15 тип с площадью

--- пробник дз/15.py
+++ пробник дз/15.py
@@ -1,6 +1,6 @@
 def f(a):
-    for x in range(1,10000):
-        for y in range(1,10000):
+    for x in range(1,1000):
+        for y in range(1,1000):
             u1 = x * y > (a + 13)
             u2 = 28 * y > 520
             u3 = x * 25 > 800
@@ -8,7 +8,6 @@ def f(a):
             if not f:
                 return False
     return True
-for a in range(1000000,1,-1):
+for a in range(-100,100):
     if f(a):
         print(a)
-        break
