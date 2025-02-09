a28ec70a0681a1fff86802ca0fcdf7beaf4000aa
polin <polinashvch248@gmail.com>
2025-02-09 17:00:54 +0200

    15 тип кодом

--- домашка (на 7 февраля)/task-15-8677.py
+++ домашка (на 7 февраля)/task-15-8677.py
@@ -1,15 +1,14 @@
-from itertools import combinations
-
-
-def f(x):
-    b = 80 <= x <= 100
-    a = a1 <= x <= a2
-    return ((x % 17) == 0) <= ((not b) or (a < (x + 30)))
+def f(a):
+    for x in range(1, 1000):
+        b = 80 <= x <= 100
+        f = ((x % 17) == 0) <= ((not b) or (a < (x + 30)))
+        if not f:
+            return False
+    return True
 
 
 ans = []
-line = [x / 10 for x in range(60 * 10, 115 * 10)]
-for a1, a2 in combinations(line, 2):
-    if all(f(x) for x in line):
-        ans.append(a2 - a1)
+for a in range(1, 10000):
+    if f(a):
+        ans.append(a)
 print(max(ans))
