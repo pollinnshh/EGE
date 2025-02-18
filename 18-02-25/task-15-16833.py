1b3ccaa843882d5eaa7ba06417723fd4be53cb87
PShevchenko <polinashvch248@gmail.com>
2025-02-18 17:46:22 +0200

    15 тип

---
+++ 18-02-25/task-15-16833.py
@@ -0,0 +1,16 @@
+from itertools import *
+
+
+def f(x):
+    p = 25 <= x <= 73
+    q = 75 <= x <= 118
+    a = a1 <= x <= a2
+    return (a and (not q)) <= (p or q)
+
+
+ans = []
+line = [x / 10 for x in range(23 * 10, 120 * 10)]  ## [x + eps for x in range(23, 120) if eps in (0, 0.1, 0.9)]
+for a1, a2 in combinations(line, 2):
+    if all(f(x) for x in line):
+        ans.append(a2 - a1)
+print(max(ans))
