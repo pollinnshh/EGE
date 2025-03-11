1b06f29ffd86bcb1a4e9852070183ea2d2050f23
PShevchenko <polinashvch248@gmail.com>
2025-03-11 17:23:04 +0200

    5 тип

---
+++ 11-03-25 (23)/task-5-7607.py
@@ -0,0 +1,9 @@
+for n in range(1,1000):
+    n1 = bin(n)[2:]
+    if n % 3 == 0:
+        n1 = n1 + n1[-3:]
+    else:
+        n1 = n1 + bin(n % 3 * 3)[2:]
+    r = int(n1, 2)
+    if r > 76:
+        print(n)
