44c0094a6e51f2dc6bef8907342c3543e8f68cf3
PShevchenko <polinashvch248@gmail.com>
2025-03-11 17:27:09 +0200

    25 тип

--- домашка (на 4 марта)/task-25-2716.py
+++ домашка (на 4 марта)/task-25-2716.py
@@ -1,8 +1,9 @@
 def f(num):
     res = []
-    for i in range(2, num // 2 + 1):
+    for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
             res.append(i)
+            res.append(num // i)
 
     res = sorted(set(res))
     if len(res) < 3:
