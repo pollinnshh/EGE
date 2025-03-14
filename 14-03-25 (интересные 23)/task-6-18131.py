da7363688f6dfa9f94ad3532e716e5a79da2ea47
PShevchenko <polinashvch248@gmail.com>
2025-03-14 17:52:43 +0200

    6 тип

---
+++ 14-03-25 (интересные 23)/task-6-18131.py
@@ -0,0 +1,29 @@
+from turtle import *
+lt(90)
+tracer(0)
+m = 15
+screensize(1000,1000)
+for i in range(9):
+    fd(22 * m)
+    rt(90)
+    fd(6 * m)
+    rt(90)
+up()
+fd(1 * m)
+rt(90)
+fd(5 * m)
+lt(90)
+down()
+for i in range(9):
+    fd(53 * m)
+    rt(90)
+    fd(75 * m)
+    rt(90)
+up()
+for x in range(-50, 50):
+    for y in range(-50, 50):
+        goto(x * m, y * m)
+        dot(4, "red")
+
+done()
+update()
