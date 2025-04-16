15c47805fd745a45f665ff0f5f3c5305bb78b13a
polin <polinashvch248@gmail.com>
2025-04-16 19:06:51 +0200

    2 тип

---
+++ homeworks/апрельский вариант_1/task-2.py
@@ -0,0 +1,14 @@
+from itertools import *
+
+
+def f(x, y, z, w):
+    return not w and ((y or z) <= ((not x) and y))
+
+
+for a1, a2, a3, a4, a5, a6, a7, a8 in product([0, 1], repeat=8):
+    table = [(a1, a2, a3, 1), (a4, a5, 1, a6), (a7, 1, 1, a8)]
+    if len(table) == len(set(table)):
+        for p in permutations("xyzw"):
+            u = [f(**dict(zip(p, t))) for t in table] == [1, 1, 1]
+            if u:
+                print(*p)
