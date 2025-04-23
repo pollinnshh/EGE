282eedcea989509688c6cf231455f7081eb5cbf9
polin <polinashvch248@gmail.com>
2025-04-24 00:54:42 +0200

    23 тип

---
+++ homeworks/апрельский вариант_2/task-23-19710.py
@@ -0,0 +1,11 @@
+def f(x, end):
+    if x == end:
+        return 1
+    if x > end:
+        return 0
+    if x == 8:
+        return 0
+    return f(x + 3, end) + f(x * 2, end)
+
+
+print(f(2, 32) * f(32, 76))
