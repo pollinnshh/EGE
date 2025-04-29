c2dd4100e266f262030d74caa9bc8a3d49e0bfe0
PShevchenko <polinashvch248@gmail.com>
2025-04-29 17:53:11 +0200

    27 тип

---
+++ lessons/29-04-25 (27 тип)/task-27-21425.py
@@ -0,0 +1,55 @@
+from math import *
+
+
+def centroid(cluster):
+    distance = []
+    for dot in cluster:
+        sum_dist = 0
+        for dot2 in cluster:
+            sum_dist += dist(dot, dot2)
+        distance.append([sum_dist, dot])
+    return min(distance)[1]
+
+
+# 1
+with open("27_A_21425.txt") as file:
+    cluster1 = []
+    cluster2 = []
+    for i in file:
+        x, y = map(float, i.split())
+        if x < 15:
+            cluster1.append((x, y))
+        else:
+            cluster2.append((x, y))
+
+center1 = centroid(cluster1)
+center2 = centroid(cluster2)
+
+ans_1 = (center1[0] + center2[0]) / 2
+ans_2 = (center1[1] + center2[1]) / 2
+
+print(int(ans_1 * 10_000), int(ans_2 * 10_000))
+
+# 2
+with open("27_B_21425.txt") as file:
+    clust1 = []
+    clust2 = []
+    clust3 = []
+    for i in file:
+        x, y = map(float, i.split())
+        if x < 0 and y < 10:
+            clust1.append((x, y))
+        elif x > 20 and y < 0:
+            clust2.append((x, y))
+        else:
+            clust3.append((x, y))
+
+cent1 = centroid(clust1)
+cent2 = centroid(clust2)
+cent3 = centroid(clust3)
+
+ans_x = (cent1[0] + cent2[0] + cent3[0]) / 3
+ans_y = (cent1[1] + cent2[1] + cent3[1]) / 3
+
+print(int(ans_x * 10_000), int(ans_y * 10_000))
+
