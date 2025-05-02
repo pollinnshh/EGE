e42d9a707221acbd8cb637c76e37a4ffe48008c9
PShevchenko <polinashvch248@gmail.com>
2025-05-02 17:48:49 +0200

    15 тип

---
+++ lessons/02-05-25 (27 тип)/task-15-16833.py
@@ -0,0 +1,17 @@
+from itertools import combinations
+
+
+def f(x):
+    p = 25 <= x <= 73
+    q = 75 <= x <= 118
+    a = a1 <= x <= a2
+    return (a and (not q)) <= (p or q)
+
+
+line = [x for x in range(25, 118)]
+ans = []
+for a1, a2 in combinations(line, 2):
+    if all(f(x) for x in line):
+        ans.append(a2 - a1)
+
+print(max(ans))
