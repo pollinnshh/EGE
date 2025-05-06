e9cf63c4b578bc4ce1bbcac8dbb983de71774bff
PShevchenko <polinashvch248@gmail.com>
2025-05-06 17:45:35 +0200

    15 тип

--- homeworks/домашка (на 6 мая)/task-15-6827.py
+++ homeworks/домашка (на 6 мая)/task-15-6827.py
@@ -10,7 +10,7 @@ def f(x):
 
 
 ans = []
-line = [x / 10 for x in range(5 * 10, 1000 * 10)]
+line = [x + eps for x in range(5, 100) for eps in [0, 0.1]]
 for a1, a2 in combinations(line, 2):
     if all(f(x) for x in line):
         ans.append(a2 - a1)
