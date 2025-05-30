15874ff41b64c12e4099ce1def341f82cbea1819
PShevchenko <polinashvch248@gmail.com>
2025-05-30 17:57:03 +0200

    26 тип

---
+++ lessons/30-05-25/task-26-2944.py
@@ -0,0 +1,21 @@
+with open("26_2944.txt") as file:
+    s, n = map(int, file.readline().split())
+    files = [int(i) for i in file]
+
+files = sorted(files)
+users = []
+for file in files:
+    if file <= s:
+        s -= file
+        users.append(file)
+    else:
+        break
+
+s += users.pop()  # удвляет последний по умолчвнию
+files = sorted(files)[::-1]
+for file in files:
+    if file <= s:
+        users.append(file)
+        break
+
+print(len(users), users[-1])
