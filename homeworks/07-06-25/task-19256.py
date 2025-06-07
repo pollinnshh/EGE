6f96932c0aa8c60cbe2812c6f9354bfb398cda92
polin <polinashvch248@gmail.com>
2025-06-07 20:53:51 +0200

    хз егэ

--- homeworks/07-06-25/task-19256.py
+++ homeworks/07-06-25/task-19256.py
@@ -4,13 +4,18 @@ with open("26_19256.txt") as file:
 
 students = sorted(students)
 ans = []
-cnt = 0
+cnt = 1
 for i in range(len(students) - 1):
     st1, st2 = students[i], students[i + 1]
+    if st1[1] == st2[1]:
+        continue
     if st1[0] == st2[0]:
         if st2[1] - st1[1] == 1:
             cnt += 1
         else:
-            ans.append([st1[0], cnt])
             cnt = 1
-print(sorted(ans, key=lambda x: (-x[1], x[0])))
+    else:
+        cnt = 1
+    ans.append([st1[0], cnt])
+
+print(sorted(ans, key=lambda x: (-x[1], x[0]))[0])
